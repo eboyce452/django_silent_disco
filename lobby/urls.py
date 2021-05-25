eb8121adeb963ec2515c16092f568189d60bdf53
eboyce452 <57389756+eboyce452@users.noreply.github.com>
2021-05-24 21:52:16 -0700

    Update urls.py

--- lobby/urls.py
+++ lobby/urls.py
@@ -5,4 +5,6 @@ from lobby import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('<str:room_name>/classification_data/', views.classifyData, name='classifydata'),
+    path('<str:room_name>/room_delete/', views.roomDelete, name='roomdelete'),
 ]
